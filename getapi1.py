4571ed50ef75cdaea98c8d44f6ff2a13c2db9cfc
Valando <brdza15@gmail.com>
2023-11-26 15:10:12 +0100

    e

--- getapi1.py
+++ getapi1.py
@@ -6,13 +6,15 @@ def get_price_kWh():
  day = now.strftime("%d")
  month = now.strftime("%m")
  year = now.strftime("%Y")
+ current_hour= now.strftime("%H")
 
  url = f"https://www.elprisenligenu.dk/api/v1/prices/{year}/{month}-{day}_DK2.json"
- 
  response = requests.get(url)
  data = response.json()
- latest_record = data[-1]
- price = latest_record.get('DKK_per_kWh')
+
+ current_record = next((record for record in data if record["time_start"].startswith(f"{year}-{month}-{day}T{current_hour}:")), None) 
+
+ price = current_record.get('DKK_per_kWh')
  print(price)
  
-get_price_kWh()      
+get_price_kWh()         
